4bca9e944d024d15938553496c384517b31f6dcf
Lucas <lucastcardoso1014@gmail.com>
2024-05-08 23:57:39 +0100

    vm

---
+++ MyLibrary/ViewModels/BookVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyLibrary.Domain.Entities;
+
+namespace MyLibrary.Web.ViewModels
+{
+    public class BookVM
+    {
+        public Book? Book { get; set; } = null!;
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? BookList { get; set; }
+    }
+}
